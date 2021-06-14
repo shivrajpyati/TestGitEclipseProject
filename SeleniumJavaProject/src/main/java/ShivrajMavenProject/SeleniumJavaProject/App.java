6d5e8436eb753f7303a9807ccc5d8ca37fe981d0
Shivraj Pyati <shivraj.pyati@gmail.com>
2021-06-15 01:41:23 +0530

    test git commit from eclipse

---
+++ SeleniumJavaProject/src/main/java/ShivrajMavenProject/SeleniumJavaProject/App.java
@@ -0,0 +1,13 @@
+package ShivrajMavenProject.SeleniumJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
